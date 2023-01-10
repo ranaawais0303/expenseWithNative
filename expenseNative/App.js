33554e97cd1820569e356dd747389a793c56e43c
Rana_awais <z.manzoor@pioneerlogics.com>
2023-01-10 01:30:17 -0800

    create and use expenseItem and formatted the date

--- expenseNative/App.js
+++ expenseNative/App.js
@@ -1,37 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { NavigationContainer } from '@react-navigation/native'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-import ManageExpenses from './screens/ManageExpenses';
-import RecentExpenses from './screens/RecentExpenses';
-import AllExpenses from './screens/AllExpenses';
-import { GlobalStyles } from './constant/styles';
-import { Ionicons } from '@expo/vector-icons'
+import { StatusBar } from "expo-status-bar";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import ManageExpenses from "./screens/ManageExpenses";
+import RecentExpenses from "./screens/RecentExpenses";
+import AllExpenses from "./screens/AllExpenses";
+import { GlobalStyles } from "./constant/styles";
+import { Ionicons } from "@expo/vector-icons";
 
-
-const Stack = createNativeStackNavigator()
-const BottomTabs = createBottomTabNavigator()
+const Stack = createNativeStackNavigator();
+const BottomTabs = createBottomTabNavigator();
 export default function App() {
-
   function ExpensesOverview() {
-    return <BottomTabs.Navigator screenOptions={{
-      headerStyle: { backgroundColor: GlobalStyles.colors.primary500 },
-      headerTintColor: 'white',
-      tabBarStyle: { backgroundColor: GlobalStyles.colors.primary500 },
-      tabBarActiveTintColor: GlobalStyles.colors.accent500
-
-    }}>
-      <BottomTabs.Screen name="RecentExpenses" component={RecentExpenses} options={{
-        title: 'Recent Expenses',
-        tabBarLabel: 'Recent',
-        tabBarIcon: ({ color, size }) => <Ionicons name="hourglass" size={size} color={color} />
-      }} />
-      <BottomTabs.Screen name="AllExpenses" component={AllExpenses} options={{
-        title: 'All Expenses',
-        tabBarLabel: 'All Expenses',
-        tabBarIcon: ({ color, size }) => <Ionicons name="calendar" size={size} color={color} />
-      }} />
-    </BottomTabs.Navigator>
+    return (
+      <BottomTabs.Navigator
+        screenOptions={{
+          headerStyle: { backgroundColor: GlobalStyles.colors.primary500 },
+          headerTintColor: "white",
+          tabBarStyle: { backgroundColor: GlobalStyles.colors.primary500 },
+          tabBarActiveTintColor: GlobalStyles.colors.accent500,
+        }}
+      >
+        <BottomTabs.Screen
+          name="RecentExpenses"
+          component={RecentExpenses}
+          options={{
+            title: "Recent Expenses",
+            tabBarLabel: "Recent",
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="hourglass" size={size} color={color} />
+            ),
+          }}
+        />
+        <BottomTabs.Screen
+          name="AllExpenses"
+          component={AllExpenses}
+          options={{
+            title: "All Expenses",
+            tabBarLabel: "All Expenses",
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="calendar" size={size} color={color} />
+            ),
+          }}
+        />
+      </BottomTabs.Navigator>
+    );
   }
 
   return (
@@ -39,13 +52,14 @@ export default function App() {
       <StatusBar style="auto" />
       <NavigationContainer>
         <Stack.Navigator>
-          <Stack.Screen name="ExpensesOverview" component={ExpensesOverview} options={{ headerShown: false }} />
+          <Stack.Screen
+            name="ExpensesOverview"
+            component={ExpensesOverview}
+            options={{ headerShown: false }}
+          />
           <Stack.Screen name="ManageExpense" component={ManageExpenses} />
         </Stack.Navigator>
       </NavigationContainer>
     </>
-
   );
 }
-
-
--- expenseNative/screens/AllExpenses.js
+++ expenseNative/screens/AllExpenses.js
@@ -1,7 +1,5 @@
-import { Text } from "react-native";
 import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput";
 function AllExpenses() {
-    return <ExpensesOutput expensesPeriod='Total' />
-
+  return <ExpensesOutput expensesPeriod="Total" />;
 }
 export default AllExpenses;
