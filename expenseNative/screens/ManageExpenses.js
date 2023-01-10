5664a217869402e49ec987df592e63a5080efbe1
Rana_awais <z.manzoor@pioneerlogics.com>
2023-01-10 02:55:25 -0800

    support for different behaviour of modal single screen behave differently

--- expenseNative/screens/ManageExpenses.js
+++ expenseNative/screens/ManageExpenses.js
@@ -1,6 +1,17 @@
+import { useLayoutEffect } from "react";
 import { Text } from "react-native";
-function ManageExpenses() {
-    return <Text> Manage Expenses Screen</Text>
+function ManageExpenses({ route, navigation }) {
+  const editedExpenseId = route.params?.expenseId;
 
+  //!! this covert into boolean
+  const isEditing = !!editedExpenseId;
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: isEditing ? "Edit Expense" : "Add Expense",
+    });
+  }, [navigation, isEditing]);
+
+  return <Text> Manage Expenses Screen</Text>;
 }
 export default ManageExpenses;
