e434bd8e232711ac193ef5f5d08a8dbb012b17b9
Rana_awais <z.manzoor@pioneerlogics.com>
2023-01-10 03:54:55 -0800

    buttons add

--- expenseNative/screens/ManageExpenses.js
+++ expenseNative/screens/ManageExpenses.js
@@ -1,5 +1,8 @@
 import { useLayoutEffect } from "react";
-import { Text } from "react-native";
+import { View, StyleSheet } from "react-native";
+import IconButton from "../components/UI/IconButton";
+import { GlobalStyles } from "../constant/styles";
+import Button from "../components/UI/Button";
 function ManageExpenses({ route, navigation }) {
   const editedExpenseId = route.params?.expenseId;
 
@@ -11,7 +14,55 @@ function ManageExpenses({ route, navigation }) {
       title: isEditing ? "Edit Expense" : "Add Expense",
     });
   }, [navigation, isEditing]);
+  function deleteExpenseHandler() {}
+  function cancelHandler() {}
+  function confirmHandler() {}
 
-  return <Text> Manage Expenses Screen</Text>;
+  return (
+    <View style={styles.container}>
+      <View style={styles.buttonContainer}>
+        <Button style={styles.button} mode="flat" onPress={cancelHandler}>
+          Cancel
+        </Button>
+        <Button style={styles.button} onPress={confirmHandler}>
+          {isEditing ? "Update" : "Add"}
+        </Button>
+      </View>
+      {isEditing && (
+        <View style={styles.deleteContainer}>
+          <IconButton
+            icon="trash"
+            color={GlobalStyles.colors.error500}
+            size={36}
+            onPress={deleteExpenseHandler}
+          />
+        </View>
+      )}
+    </View>
+  );
 }
 export default ManageExpenses;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: GlobalStyles.colors.primary800,
+  },
+  button: {
+    minWidth: 120,
+    marginHorizontal: 8,
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  deleteContainer: {
+    marginTop: 16,
+    paddingTop: 8,
+    borderTopWidth: 2,
+    borderBottomColor: GlobalStyles.colors.primary200,
+    alignItems: "center",
+  },
+});
