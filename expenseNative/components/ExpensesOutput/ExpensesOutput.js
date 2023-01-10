2890154916e92d49e2604524bb6d781048dea5b9
Rana_awais <z.manzoor@pioneerlogics.com>
2023-01-09 23:26:17 -0800

    style summary and expenseOutput component

--- expenseNative/components/ExpensesOutput/ExpensesOutput.js
+++ expenseNative/components/ExpensesOutput/ExpensesOutput.js
@@ -1,6 +1,7 @@
-import { Text, View, FlatList } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 import ExpensesList from "./ExpensesList";
 import ExpensesSummary from "./ExpensesSummary";
+import { GlobalStyles } from "../../constant/styles";
 
 const DUMMY_EXPENSES = [
   {
@@ -67,7 +68,7 @@ const DUMMY_EXPENSES = [
 
 function ExpensesOutput({ expenses, expensesPeriod }) {
   return (
-    <View>
+    <View style={styles.container}>
       <ExpensesSummary expenses={DUMMY_EXPENSES} periodName={expensesPeriod} />
       <ExpensesList expenses={DUMMY_EXPENSES} />
     </View>
@@ -75,3 +76,11 @@ function ExpensesOutput({ expenses, expensesPeriod }) {
 }
 
 export default ExpensesOutput;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: GlobalStyles.colors.primary700,
+  },
+});
